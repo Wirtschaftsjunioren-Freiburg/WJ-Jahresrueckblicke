58d356921649bed15ce03075aa1d7f81bc1fc852
Daniel Kemen <dk@software-design.de>
2023-03-06 19:41:32 +0100

    - Fix Import script

--- imports/import.py
+++ imports/import.py
@@ -1,11 +1,7 @@
 startYear = 1953
 endYear = 2022
-from .models import *
 
-for (i in range(startYear, endYear + 1)) {
+from webpage.models import YearSlide
+
+for i in range(startYear, endYear + 1):
     YearSlide.objects.create(year=i)
-}   
-
-import .importEhrungen
-import .importVorsitzende
-import .importVorstand
