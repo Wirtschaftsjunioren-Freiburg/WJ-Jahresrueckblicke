be8ca30c6356cc5df5bf698c4281727dd080c200
Daniel Kemen <dk@software-design.de>
2023-05-25 08:44:57 +0200

    - Fix umlauts

--- jahresrueckblick/webpage/tools.py
+++ jahresrueckblick/webpage/tools.py
@@ -19,5 +19,5 @@ def cropImage(original):
         else:
             image = image.crop((0, (height-width*9/16)/2, width, height-(height-width*9/16)/2))
         image.thumbnail((1200, 1200), Image.ANTIALIAS)
-        image.save(settings.MEDIA_ROOT+'cropped/' + original.url.replace(settings.MEDIA_URL, ''))
-        return original.url.replace(settings.MEDIA_URL, settings.MEDIA_URL+'cropped/')
+        image.save(settings.MEDIA_ROOT+'cropped/' + original.url.replace(settings.MEDIA_URL, '').replace(['ä', 'ö', 'ü', 'ß'], ['ae', 'oe', 'ue', 'ss']))
+        return original.url.replace(settings.MEDIA_URL, settings.MEDIA_URL+'cropped/').replace(['ä', 'ö', 'ü', 'ß'], ['ae', 'oe', 'ue', 'ss'])
